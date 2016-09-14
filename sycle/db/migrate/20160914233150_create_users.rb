bbb71a046dd753367f597edb0c946e2ebd8fd9a1
BenGoldstein88 <bengoldsteinmusic@gmail.com>
2016-09-14 16:53:47 -0700

    setup API skeleton and user model with admin privelges

---
+++ sycle/db/migrate/20160914233150_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+    	t.string :username, null: false
+    	t.string :password_hash, null: false
+    	t.string :first_name
+    	t.string :last_name
+    	t.string :address
+    	t.integer :zip_code
+    	t.string :email, null: false
+        t.boolean :admin, default: false
+    	t.timestamps null: false
+    end
+  end
+end
